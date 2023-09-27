ec03d1cc00d5c85fa52ac4d01da60d8a31cf8c13
aikooo7 <prozinhopro1973@gmail.com>
2023-09-27 17:16:35 +0100

    Added: Comments.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,17 @@ use hex::encode;
 use sha2::{Digest, Sha512};
 
 fn main() {
+    // Initialize variables
     let mut input: String;
     let mut hasher = Sha512::new();
     let mut buffer = [0; 1024];
 
-    let filepath = user_input("What file you want to hash? ");
+    // Get the file path from user input
+    let filepath = user_input("What file do you want to hash? ");
     let filename = File::open(&filepath).expect("Unable to open file.");
     let mut reader = BufReader::new(filename);
+
+    // Read and hash the file in chunks
     loop {
         let counter = reader
             .read(&mut buffer)
@@ -25,31 +29,33 @@ fn main() {
         hasher.update(&buffer[..counter]);
     }
 
+    // Finalize the hash and display it in hexadecimal
     let hash = hasher.finalize();
     let hex_hash = encode(hash);
     println!("Your hash: {}", hex_hash);
 
+    // Prompt user to write the hash to a file
     input = user_input("Want to write the hash to a file? ");
 
     match input.to_lowercase().as_str() {
         "yes" => {
             println!("Done...");
-            let mut f = File::options()
-                .append(true)
-                .create(true)
-                .open("hashes.txt")
-                .expect("Opening hashes file.");
+
+            // Open or create the "hashes.txt" file and append the hash information
+            let mut f = File::create("hashes.txt").expect("Opening hashes file.");
             writeln!(&mut f, "Filename: {} | Hash: {}", filepath, hex_hash)
                 .expect("Writing to the hashes file");
         }
-        "no" => println!("Not writting to the file..."),
+        "no" => println!("Not writing to the file..."),
         _ => print!("Input needs to be either yes or no."),
     }
 
-    input = user_input("Want to hash other file? ");
+    // Prompt user to hash another file
+    input = user_input("Want to hash another file? ");
 
     match input.to_lowercase().as_str() {
         "yes" => {
+            // Clear the terminal and call the main function again for another run
             print!("\x1B[2J\x1B[1;1H");
             main();
         }
@@ -58,6 +64,7 @@ fn main() {
     }
 }
 
+// Function to get user input
 fn user_input(message: &str) -> String {
     let mut input = String::new();
 
